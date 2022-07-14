15e51058454708b2c4f495a939985afabe580df5
pinnikolas <pinnikolay.work@gmail.com>
2022-07-14 12:05:59 +0300

    add files and header style

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const burgerIcon = document.querySelector('.burger__icon');
+const body = document.body;
+
+if (burgerIcon) {
+    const menu = document.querySelector('.menu__body');
+
+    burgerIcon.addEventListener("click", () => {
+        burgerIcon.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('lock');
+    });
+
+    menu.querySelectorAll('.menu__link').forEach(link => {
+        link.addEventListener("click", () => {
+            body.classList.remove('lock');
+            burgerIcon.classList.remove('active');
+            menu.classList.remove('active');
+        })
+    })
+}
+
+// *========================================================*
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substring(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+});
